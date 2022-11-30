6254481f3c43240ad15aff414bf8314d3dbd1021
Мария Ларичева <larichevams@gmail.com>
2022-11-30 21:13:39 +0300

    Добавьте файлы проекта.

---
+++ calc3.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+    double firstVar, secondVar;
+    char operation;
+
+    std::cout << "Enter first number: ";
+    std::cin >> firstVar;
+    std::cout << "Enter second number: ";
+    std::cin >> secondVar;
+    std::cout << "Enter operation : ";
+    std::cin >> operation;
+
+    switch (operation) {
+    case '-':
+        std::cout << firstVar << "-" << secondVar << "=" << firstVar - secondVar << '\n';
+        break;
+    case '+':
+        std::cout << firstVar << "+" << secondVar << "=" << firstVar + secondVar << '\n';
+        break;
+    case '*':
+        std::cout << firstVar << "*" << secondVar << "=" << firstVar * secondVar << '\n';
+        break;
+    case '/':
+        std::cout << firstVar << "/" << secondVar << "=" << firstVar / secondVar << '\n';
+        break;
+    case '^':
+        std::cout << firstVar << "^" << secondVar << "=" << std::pow(firstVar, secondVar) << '\n';
+        break;
+    default:
+        std::cout << "Error operation\n";
+    }
+    return 0;
+}
+
